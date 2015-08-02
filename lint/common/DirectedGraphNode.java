f6391c058d366847756fb62cf1b87b20446f28e9
lxming <xiaoming.liu@gmail.com>
2015-08-02 15:38:33 -0400

    find_the_weak_connected_component

---
+++ lint/common/DirectedGraphNode.java
@@ -0,0 +1,13 @@
+package lint.common;
+
+import java.util.ArrayList;
+
+public class DirectedGraphNode {
+    public int label;
+    public ArrayList<DirectedGraphNode> neighbors;
+
+    public DirectedGraphNode(int x) {
+        label = x;
+        neighbors = new ArrayList<DirectedGraphNode>();
+    }
+}
